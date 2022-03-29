21b8edee744674e5cfc4bcb89ed3cd1c9ca2e87b
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-29 18:03:56 +0300

    Generate test console command 2022-03-29

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test command for artisan';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('This is laravel artisan command example');
+        Log::info('Artisan Test command was executed!');
+    }
+}
