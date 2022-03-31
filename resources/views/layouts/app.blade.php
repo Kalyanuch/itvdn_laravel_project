58c869258dfb59c208c952d34a1e6e583c8a89c0
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-31 16:54:03 +0300

    Add footer 2022-03-31

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,13 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+            <div class="footer-copyright py-3">
+                &copy; 2022 Copyright:
+                <a href="{{ route('index') }}">{{ config('app.name') | 'Laravel' }}</a>
+            </div>
+        </footer>
     </div>
     <!-- Scripts -->
     <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}" defer></script>
