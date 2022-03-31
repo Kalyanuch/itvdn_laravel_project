ae5d3f0551501cb997efd8a39b922a6bd7274e4c
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-31 19:32:29 +0300

    Product gallery relation 2022-03-31

--- app/Gallery.php
+++ app/Gallery.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     protected $fillable = ['product_id', 'photos'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
