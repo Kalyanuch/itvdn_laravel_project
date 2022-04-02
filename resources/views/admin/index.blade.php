1f4606484bee45d52c8c0c436bdc3f4dfab01e36
Shamrylo Mykola <itlutsk@gmail.com>
2022-04-02 12:32:52 +0300

    Add admin lte default template 2022-04-02

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,18 @@
-@extends('layouts.app')
+@extends('admin.layout')
 
 @section('title')
 {{ __('Admin index') }}
 @endsection
 
 @section('content')
-<h1>{{ __('This is main admin page') }}</h1>
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Welcome</h3>
+            </div>
+            <div class="card-body">
+                <h1>Welcome</h1>
+            </div>
+        </div>
+    </div>
 @endsection
